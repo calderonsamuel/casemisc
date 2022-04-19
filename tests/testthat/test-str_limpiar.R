ae82b3216e5a717cca421e17e1f0e1eaf36dd0aa
calderonsamuel <samuel.calderon@uarm.pe>
2022-04-19 14:29:55 -0500

    add test for NA

--- tests/testthat/test-str_limpiar.R
+++ tests/testthat/test-str_limpiar.R
@@ -1,4 +1,5 @@
 test_that("str_limpiar funciona", {
     expect_equal(str_limpiar("un\t\tavión"), "UN AVION")
     expect_equal(str_limpiar("universidad de   huánuco"), "UNIVERSIDAD DE HUANUCO")
+    expect_true(is.na(str_limpiar(NA)))
 })
