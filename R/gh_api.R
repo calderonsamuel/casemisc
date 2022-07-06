d8cd516b758e696a894d909d457c0bd32e5fbcbb
Samuel Calderon <samuel.calderon@uarm.pe>
2022-07-06 09:15:52 -0500

    get issues from github repo

---
+++ R/gh_api.R
@@ -0,0 +1,13 @@
+gh_repos_issues <- function(owner = "calderonsamuel", repo = ".", state = "open") {
+    repo <- if (repo == ".") basename(here::here()) else repo
+
+    gh::gh(
+        "/repos/{owner}/{repo}/issues",
+        owner = owner,
+        repo = repo,
+        state = match.arg(state, c("open", "closed", "all"))
+    )
+}
+
+
+
